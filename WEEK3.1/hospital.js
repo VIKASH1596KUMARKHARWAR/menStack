d33d3da8bdb3ff19f8a8b6675f91d9583692812a
Vikash kumar kharwar <amit.sbz1596@gmail.com.com>
2024-08-30 16:27:27 +0530

    Add node_modules to .gitignore and remove it from tracking

---
+++ WEEK3.1/hospital.js
@@ -0,0 +1,32 @@
+//next keyword(as a parameter to the function-> acting as middleware )
+//ensure that if the pre-checks is done and passed then move the cursor the next segments  ie. to net middleware or to to computation logic
+
+//something that is sitting between the router and the handler
+
+const express = require("express");
+
+const app = express();
+
+app.get("/health-checkup", function (req, res) {
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const kidneyId = req.query.kidneyId;
+
+  if (!(username === "harkirat" && password === "pass")) {
+    res.status(400).json({ "msg": "Something up with your inputs" })
+    return
+  }
+  if (kidneyId != 1 && kidneyId != 2) {
+    res.status(400).json({ "msg": "Something up with your kidneys" })
+    return;
+  }
+  
+  //do something with code
+  res.json({
+    msg: "Your Kidney is fine !",
+  });
+});
+
+app.listen(3000);
+
+
--- WEEK3/middleWare/hospital.js
+++
@@ -1,4 +0,0 @@
-//next keyword(as a parameter to the function-> actiong as middleware )
-//ensure that if the pre-checks is done and passed then move the cursor the next seqment  ie. to net middlewarre or to to computiaon logic
-
-//something that is sitting between the router and the handeler
