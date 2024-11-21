10b1a4ea921a0ec20df344902edd30581b7dcb55
Vikash kumar kharwar <amit.sbz1596@gmail.com.com>
2024-11-21 06:29:57 +0530

    "Updated hospital.js to add port variable and console log, added zod dependency to package.json and package-lock.json"

--- WEEK3.1/hospital.js
+++ WEEK3.1/hospital.js
@@ -1,11 +1,12 @@
 //next keyword(as a parameter to the function-> acting as middleware )
-//ensure that if the pre-checks is done and passed then move the cursor the next segments  ie. to net middleware or to to computation logic
+//ensure that if the pre-checks is done and passed then move the cursor  to the next segments  ie. to next middleware or to to computation logic
 
 //something that is sitting between the router and the handler
 
 const express = require("express");
 
 const app = express();
+const port = 3000;
 
 app.get("/health-checkup", function (req, res) {
   const username = req.headers.username;
@@ -27,6 +28,7 @@ app.get("/health-checkup", function (req, res) {
   });
 });
 
-app.listen(3000);
-
+app.listen(port,()=>{
+  console.log(`server is running on the port ${port}`);
+});
 
